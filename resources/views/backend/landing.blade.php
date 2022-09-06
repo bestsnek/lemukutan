90da2ebd5e105aa79d7b4ea05b8252b4f64bd26a
bestsnek <degakurniawansaputra@gmail.com>
2022-09-06 12:07:22 +0700

    install bootstrap, basic backend

---
+++ resources/views/backend/landing.blade.php
@@ -0,0 +1,12 @@
+@extends('backend.b_layouts.main')
+
+@section('content')
+
+<div class="container-fluid">
+    <h1 class="text-center">
+        Sistem Informasi Untuk Pulau Lemukutan
+    </h1>
+    
+</div>
+
+@endsection
