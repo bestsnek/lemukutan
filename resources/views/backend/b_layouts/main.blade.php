51cba517348bec868ec51661be2f864b4ab2cae4
bestsnek <degakurniawansaputra@gmail.com>
2022-09-09 01:38:32 +0700

    basic navbar

--- resources/views/backend/b_layouts/main.blade.php
+++ resources/views/backend/b_layouts/main.blade.php
@@ -7,22 +7,24 @@
     {{-- <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}> --}}
 
     <title>Lemukutan Informasi</title>
-    {{-- @include("site.layouts.backend.partial.navbar") --}}
+    @include("backend.b_layouts.navbar")
     
     {{-- bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
     {{-- style sendiri --}}
     {{-- <link rel="stylesheet" href="{{ asset('css/style.css')}}"> --}}
-
-
-   
-
+    
+    
+    
+    
 </head>
 <body>
-    
-    @yield("content")
-
+    <main>
+        
+        
+        @yield("content")
+    </main>
 </body>
 
 <footer>
