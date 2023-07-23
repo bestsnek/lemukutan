d189c89d89d19b27aa13b07e29eda4c896ae214f
bestsnek <degakurniawansaputra@gmail.com>
2023-07-23 08:08:27 +0700

    fix

--- resources/views/backend/landing.blade.php
+++ resources/views/backend/landing.blade.php
@@ -9,42 +9,39 @@
             <div class="text-center">
                 <h4> Daftar destinasi dan QR </h1>
             </div>
-            
-                <table class="table table-hover">
-                    <a href="{{ route('backend.form_buat_landmark') }}" class="btn btn-sm btn-primary float-right">Tambah Landmark</a>
-                    
-                    <thead>
-                        <tr>
-                            <th>id</th>
-                            <th>Nama Landmark</th>
-                            <th>QR</th>
-                            <th>Tipe Tempat</th>
-                            <th>Status</th>
-                            <th>Aksi</th>
-                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($landmark as $lan)
-                        <tr>
-                            <td>{{$lan->id}}</td>
-                            <td>{{$lan->nama}}</td>
-                            <td><a href="{{route('backend.lihat_qrcode', ['qrcode' => $lan->qrCode] ) }}" class="btn btn-sm btn-info" >Klik untuk lihat QRCode</a></td>
-                            <td>{{ $lan->isHarbor == 1 ? "Dermaga" : "Bukan Dermaga" }}</th>
-                            <td>
-                                <p style={{ $lan->active == 1 ? "color:green" : "color:red"  }} >{{ $lan->active == 1 ? "aktif" : "tidak aktif" }} 
-                                <a href="{{route('backend.ubah_status',['id'=>$lan->id])}}" class="btn btn-sm {{ $lan->active == 1 ? "btn-success" : "btn-danger"  }} btn-success" onclick="return confirm('Apakah anda yakin ingin mengganti status landmark?');"> Ganti </a>
-                                </p>
-                            </th>
-                            <td><a href="{{route('backend.details', ['id'=> $lan->id]) }}" class="btn btn-sm btn-info">Detail</a></td>
-                            
-                        </tr>
-                        @endforeach
+            <table class="table table-hover">
+                <a href="{{ route('backend.form_buat_landmark') }}" class="btn btn-sm btn-primary float-right">Tambah Landmark</a>
+                
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Nama Landmark</th>
+                        <th>QR</th>
+                        <th>Tipe Tempat</th>
+                        <th>Status</th>
+                        <th>Aksi</th>
                         
-                    </tbody>
-                </table> 
-              
-           
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($landmark as $lan)
+                    <tr>
+                        <td>{{$lan->id}}</td>
+                        <td>{{$lan->nama}}</td>
+                        <td><a href="{{route('backend.lihat_qrcode', ['qrcode' => $lan->qrCode] ) }}" class="btn btn-sm btn-info" >Klik untuk lihat QRCode</a></td>
+                        <td>{{ $lan->isHarbor == 1 ? "Dermaga" : "Bukan Dermaga" }}</th>
+                        <td>
+                            <p style={{ $lan->active == 1 ? "color:green" : "color:red"  }} >{{ $lan->active == 1 ? "aktif" : "tidak aktif" }} 
+                            <a href="{{route('backend.ubah_status',['id'=>$lan->id])}}" class="btn btn-sm {{ $lan->active == 1 ? "btn-success" : "btn-danger"  }} btn-success" onclick="return confirm('Apakah anda yakin ingin mengganti status landmark?');"> Ganti </a>
+                            </p>
+                        </th>
+                        <td><a href="{{route('backend.details', ['id'=> $lan->id]) }}" class="btn btn-sm btn-info">Detail</a></td>
+                        
+                    </tr>
+                    @endforeach
+                    
+                </tbody>
+            </table> 
         </div>
     </div>
 </div>
